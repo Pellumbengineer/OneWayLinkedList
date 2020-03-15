dce89ef28a473afe9569a02fd69aa343e55f34a9
Pellumbengineer <pellumbbaboci@gmail.com>
2020-03-15 14:51:09 +0300

    All interface methods done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,50 +4,61 @@ import java.util.*;
 
 interface IList<E> extends Iterable<E> {
     boolean add(E e); // qdd element to the end of list
+
     void add(int index, E element) throws NoSuchElementException; // add element on position index
+
     void clear(); // delete all elements
+
     boolean contains(E element); // is list containing an element (equals())
+
     E get(int index) throws NoSuchElementException; //get element from position
+
     E set(int index, E element) throws NoSuchElementException; // set new value on position
+
     int indexOf(E element); // where is element (equals())
+
     boolean isEmpty();
+
     Iterator<E> iterator();
+
     ListIterator<E> listIterator() throws UnsupportedOperationException; // for ListIterator
+
     E remove(int index) throws NoSuchElementException; // remove element from position index
+
     boolean remove(E e); // remove element
+
     int size();
 }
 
-class OneWayLinkedList<E> implements IList<E>{
+class OneWayLinkedList<E> implements IList<E> {
+    int size = 0;
 
-    private class Element{
+    // Node
+    private class Element {
         public Element(E e) {
-            this.object=e;
+            this.object = e;
         }
+
         E object;
-        Element next=null;
+        Element next = null;
     }
 
     Element sentinel;
 
     private class InnerIterator implements Iterator<E> {
-        // TODO
         Element actElement;
 
         public InnerIterator() {
-            // TODO
             actElement = sentinel.next;
         }
 
         @Override
         public boolean hasNext() {
-            // TODO
             return actElement != null;
         }
 
         @Override
         public E next() {
-            // TODO
             E value = actElement.object;
             actElement = actElement.next;
             return value;
@@ -55,16 +66,7 @@ class OneWayLinkedList<E> implements IList<E>{
     }
 
     public OneWayLinkedList() {
-        // make a sentinel
-        // TODO
         sentinel = new Element((E) "abc");
-    }
-    public OneWayLinkedList(String x) {
-        // make a sentinel
-        // TODO
-        sentinel = new Element((E) "abc");
-        sentinel.next = new Element((E) x);
-
     }
 
     @Override
@@ -79,113 +81,154 @@ class OneWayLinkedList<E> implements IList<E>{
 
     @Override
     public boolean add(E e) {
-        // TODO Auto-generated method stub
-
         Element newElem = new Element(e);
-
         Element tail = sentinel.next;
         while (tail.next != null)
             tail = tail.next;
         tail.next = newElem;
+        size = size + 1;
         return true;
     }
 
     @Override
     public void add(int index, E element) throws NoSuchElementException {
-        // TODO Auto-generated method stub
-        Element newElem = new Element(element);
-        Element e = sentinel.next;
-        for(int i = 0; i < index; i++) {
-            e = e.next;
-        }
-        e.next = newElem;
-        newElem.next = e.next.next;
-
+        set(index, element);
     }
 
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
-
+        Element e = sentinel;
+        while (iterator().hasNext()) {
+            e.next = null;
+        }
     }
 
     @Override
     public boolean contains(E element) {
-        // TODO Auto-generated method stub
+        while (iterator().hasNext()) {
+            if (iterator().next() == element) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public E get(int index) throws NoSuchElementException {
-        // TODO Auto-generated method stub
-        return null;
+        Element e = sentinel.next;
+        if (e == null) {
+            return null;
+        }
+        for (int i = 0; i < index; i++) {
+            e = e.next;
+        }
+        return e.object;
     }
 
     @Override
     public E set(int index, E element) throws NoSuchElementException {
-        // TODO Auto-generated method stub
-        return null;
+        Element newElem = new Element(element);
+        Element e = sentinel.next;
+        for (int i = 0; i < index; i++) {
+            e = e.next;
+        }
+        e.next = newElem;
+        newElem.next = e.next.next;
+        return newElem.object;
     }
 
     @Override
     public int indexOf(E element) {
-        // TODO Auto-generated method stub
-        return 0;
+        int pos = 0;
+        while (iterator().hasNext()) {
+            pos = pos + 1;
+            if (iterator().next() == element) {
+                break;
+            }
+        }
+        return pos;
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
+        if (sentinel.next == null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public E remove(int index) throws NoSuchElementException {
-        // TODO Auto-generated method stub
-        return null;
+        Element e = sentinel.next;
+        Element deleted = null;
+        Element prev = sentinel;
+        if (isEmpty()) {
+            return null;
+        }
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+            e = e.next;
+        }
+        prev.next = e.next;
+        deleted = e;
+        e = null;
+        return deleted.object;
     }
 
     @Override
     public boolean remove(E e) {
-        // TODO Auto-generated method stub
-        return false;
+        int pos = 0;
+        E deletedElm = null;
+        while (iterator().hasNext()) {
+            pos++;
+            if (iterator().next() == e) {
+                deletedElm = remove(pos);
+                break;
+            }
+        }
+        if (deletedElm == e) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        int pos=0;
-        Element actElem=sentinel.next;
-        while(actElem!=null)
-        {
-            pos++;
-            actElem=actElem.next;
+        int size = 0;
+        Element e = sentinel.next;
+        while (e != null) {
+            size++;
+            e = e.next;
         }
-        return pos;
-
+        return size;
     }
-
 }
 
 
-class Link{
+class Link {
     public String ref;
+
     public Link(String ref) {
-        this.ref=ref;
+        this.ref = ref;
     }
     // in the future there will be more fields
 }
 
-class Document{
+class Document {
     public String name;
     public OneWayLinkedList<Link> links;
+
     public Document(String name, Scanner scan) {
         // TODO
         load(scan);
     }
+
     public void load(Scanner scan) {
         //TODO
     }
+
     // accepted only small letters, capitalic letter, digits nad '_' (but not on the begin)
     private static boolean correctLink(String link) {
         return true;
@@ -201,127 +244,118 @@ class Document{
 public class Main {
 
 
-
-
     static Scanner scan; // for input stream
-
-
-
 
 
     public static void main(String[] args) {
         System.out.println("START");
-        scan=new Scanner(System.in);
-        Document[] doc=null;
-        int currentDocNo=0;
-        int maxNo=-1;
-        boolean halt=false;
-        while(!halt) {
-            String line=scan.nextLine();
+        scan = new Scanner(System.in);
+        Document[] doc = null;
+        int currentDocNo = 0;
+        int maxNo = -1;
+        boolean halt = false;
+        while (!halt) {
+            String line = scan.nextLine();
             // empty line and comment line - read next line
-            if(line.length()==0 || line.charAt(0)=='#')
+            if (line.length() == 0 || line.charAt(0) == '#')
                 continue;
             // copy line to output (it is easier to find a place of a mistake)
-            System.out.println("!"+line);
-            String word[]=line.split(" ");
+            System.out.println("!" + line);
+            String word[] = line.split(" ");
             // go n - start with array of the length n
-            if(word[0].equalsIgnoreCase("go") && word.length==2) {
-                maxNo=Integer.parseInt(word[1]);
+            if (word[0].equalsIgnoreCase("go") && word.length == 2) {
+                maxNo = Integer.parseInt(word[1]);
                 doc = new Document[maxNo];
                 continue;
             }
             //ch - change index
-            if(word[0].equalsIgnoreCase("ch") && word.length==2) {
-                currentDocNo=Integer.parseInt(word[1]);
+            if (word[0].equalsIgnoreCase("ch") && word.length == 2) {
+                currentDocNo = Integer.parseInt(word[1]);
                 continue;
             }
 
             // ld documentName
-            if(word[0].equalsIgnoreCase("ld") && word.length==2) {
-                doc[currentDocNo]=new Document(word[1],scan);
+            if (word[0].equalsIgnoreCase("ld") && word.length == 2) {
+                doc[currentDocNo] = new Document(word[1], scan);
                 continue;
             }
             // ha
-            if(word[0].equalsIgnoreCase("ha") && word.length==1) {
-                halt=true;
+            if (word[0].equalsIgnoreCase("ha") && word.length == 1) {
+                halt = true;
                 continue;
             }
             // clear
-            if(word[0].equalsIgnoreCase("clear") && word.length==1) {
+            if (word[0].equalsIgnoreCase("clear") && word.length == 1) {
                 doc[currentDocNo].links.clear();
                 continue;
             }
             // show
-            if(word[0].equalsIgnoreCase("show") && word.length==1) {
+            if (word[0].equalsIgnoreCase("show") && word.length == 1) {
                 System.out.println(doc[currentDocNo].toString());
                 continue;
             }
             // size
-            if(word[0].equalsIgnoreCase("size") && word.length==1) {
+            if (word[0].equalsIgnoreCase("size") && word.length == 1) {
                 System.out.println(doc[currentDocNo].links.size());
                 continue;
             }
             // add str
-            if(word[0].equalsIgnoreCase("add") && word.length==2) {
+            if (word[0].equalsIgnoreCase("add") && word.length == 2) {
                 System.out.println(doc[currentDocNo].links.add(new Link(word[1])));
                 continue;
             }
             // addi index str
-            if(word[0].equalsIgnoreCase("addi") && word.length==3) {
-                int index=Integer.parseInt(word[1]);
+            if (word[0].equalsIgnoreCase("addi") && word.length == 3) {
+                int index = Integer.parseInt(word[1]);
                 try {
                     doc[currentDocNo].links.add(index, new Link(word[2]));
-                }
-                catch (NoSuchElementException e) {
+                } catch (NoSuchElementException e) {
                     System.out.println("error");
                 }
                 continue;
             }
             // get index
-            if(word[0].equalsIgnoreCase("get") && word.length==2) {
-                int index=Integer.parseInt(word[1]);
+            if (word[0].equalsIgnoreCase("get") && word.length == 2) {
+                int index = Integer.parseInt(word[1]);
                 try {
-                    Link l=doc[currentDocNo].links.get(index);
+                    Link l = doc[currentDocNo].links.get(index);
                     System.out.println(l.ref);
-                }
-                catch(NoSuchElementException e) {
+                } catch (NoSuchElementException e) {
                     System.out.println("error");
                 }
                 continue;
             }
             // set index str
-            if(word[0].equalsIgnoreCase("set") && word.length==3) {
-                int index=Integer.parseInt(word[1]);
+            if (word[0].equalsIgnoreCase("set") && word.length == 3) {
+                int index = Integer.parseInt(word[1]);
                 try {
-                    Link l=doc[currentDocNo].links.set(index,new Link(word[2]));
+                    Link l = doc[currentDocNo].links.set(index, new Link(word[2]));
                     System.out.println(l.ref);
-                }
-                catch(NoSuchElementException e) {
+                } catch (NoSuchElementException e) {
                     System.out.println("error");
                 }
 
                 continue;
             }
             // index str
-            if(word[0].equalsIgnoreCase("index") && word.length==2) {
-                int index=doc[currentDocNo].links.indexOf(new Link(word[1]));
+            if (word[0].equalsIgnoreCase("index") && word.length == 2) {
+                int index = doc[currentDocNo].links.indexOf(new Link(word[1]));
                 System.out.println(index);
                 continue;
             }
             // remi index
-            if(word[0].equalsIgnoreCase("remi") && word.length==2) {
-                int index=Integer.parseInt(word[1]);
+            if (word[0].equalsIgnoreCase("remi") && word.length == 2) {
+                int index = Integer.parseInt(word[1]);
                 try {
-                    Link l=doc[currentDocNo].links.remove(index);
+                    Link l = doc[currentDocNo].links.remove(index);
                     System.out.println(l.ref);
-                }
-                catch(NoSuchElementException e) {
+                } catch (NoSuchElementException e) {
                     System.out.println("error");
                 }
                 continue;
             }
             // rem str
-            if(word[0].equalsIgnoreCase("rem") && word.length==2) {
+            if (word[0].equalsIgnoreCase("rem") && word.length == 2) {
                 System.out.println(doc[currentDocNo].links.remove(new Link(word[1])));
                 continue;
             }
@@ -331,8 +365,6 @@ public class Main {
         scan.close();
 
     }
-
-
 
 
 }
